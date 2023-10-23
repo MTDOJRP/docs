e7a9d864f21ef98c00d636b9b9431df58768bf8b
GamercraftStudios <58826382+BurnhamR@users.noreply.github.com>
2023-10-23 12:25:14 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,7 +30,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mtdojrp/docs',
+            'https://github.com/MTDOJRP/docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
