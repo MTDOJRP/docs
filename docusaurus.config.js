1e5a5965820b1461c3b4c858693c772099ad300f
burnhamr <skyy@skyy.cc>
2023-10-23 11:47:28 -0600

    New thing!

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'MTDOJRP',
+  tagline: 'Montana Department of Justice Roleplay Documentation',
+  favicon: 'img/favicon.ico',
+  url: 'https://docs.mtdojrp.org',
+  baseUrl: '/',
+  organizationName: 'MTDOJRP',
+  projectName: 'docs',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/mtdojrp/docs',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/MTDOJRP.png',
+      navbar: {
+        title: 'MTDOJRP',
+        logo: {
+          alt: 'MTDOJRP',
+          src: 'img/MTDOJRP.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {
+            href: 'https://mtdojrp.org',
+            label: 'MTDOJRP',
+            position: 'right',
+          },
+          {
+            href: 'https://CAD.mtdojrp.org',
+            label: 'CAD',
+            position: 'right',
+          },
+          {
+            href: 'https://mtdojrp.org',
+            label: 'Discord',
+            position: 'right',
+          }
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} MTDOJRP.`,
+      },
+      prism: {
+        theme: darkCodeTheme,
+        lightTheme: lightCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
