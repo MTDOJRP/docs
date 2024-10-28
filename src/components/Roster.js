91a6720998c8a1f1b0178755c834ef52c55d0872
burnhamr <skyy@skyy.cc>
2024-10-28 12:53:55 -0600

    Added rank/role updating & Updated Roster Ranks

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -9,21 +9,21 @@ const [rosterData, setRosterData] = useState({
     lspd: []
 });
 const staticSahpRoster = [
-    { callSign: '1C-01', rank: 'Commissioner', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
-    { callSign: '1C-02', rank: 'Asst. Commissioner', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
-    { callSign: '1C-03', rank: 'Captain', status: 'Vacant', role: 'Division Management', startDate: '', fto: '', hours: '' },
-    { callSign: '1L-04', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-01', rank: 'Colonel', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-02', rank: 'Lieutenant colonel', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-03', rank: 'Major', status: 'Vacant', role: 'Department Management', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-04', rank: 'Captain', status: 'Vacant', role: 'Division Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '1L-05', rank: 'Lieutenant', status: 'Vacant', role: 'LIA Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1L-06', rank: 'Lieutenant', status: 'Vacant', role: 'TEU Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1L-06', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '1S-07', rank: 'Sergeant', status: 'Vacant', role: 'Patrol Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '1S-08', rank: 'Sergeant', status: 'Vacant', role: 'Shift Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '1S-09', rank: 'Sergeant', status: 'Vacant', role: 'Shift Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-10', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-11', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-12', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-13', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-14', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1V-15', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-10', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-11', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-12', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-13', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-14', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '1V-15', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
     { callSign: '1U-16', rank: 'Senior Trooper', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '1U-17', rank: 'Senior Trooper', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '1U-18', rank: 'Senior Trooper', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
@@ -38,26 +38,26 @@ const staticSahpRoster = [
     { callSign: '1U-27', rank: 'Trooper 2nd Class', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '1U-28', rank: 'Trooper 2nd Class', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '1U-29', rank: 'Trooper 2nd Class', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
-    { callSign: '1U-30', rank: 'Trooper 2nd Class', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
+    { callSign: '1U-30', rank: 'Probationary Trooper', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '1N-31', rank: 'Probationary Trooper', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '1N-32', rank: 'Probationary Trooper', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
 ];
 const staticBcsoRoster = [
     { callSign: '2C-01', rank: 'Sheriff', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
     { callSign: '2C-02', rank: 'Undersheriff', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
-    { callSign: '2C-03', rank: 'Captain', status: 'Vacant', role: 'Division Management', startDate: '', fto: '', hours: '' },
-    { callSign: '1L-04', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2L-05', rank: 'Lieutenant', status: 'Vacant', role: 'LIA Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '1L-06', rank: 'Lieutenant', status: 'Vacant', role: 'Patrol Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '2C-03', rank: 'Chief Deputy', status: 'Vacant', role: 'Department Management', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-04', rank: 'Captain', status: 'Vacant', role: 'Division Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1L-05', rank: 'Lieutenant', status: 'Vacant', role: 'LIA Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1L-06', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '2S-07', rank: 'Sergeant', status: 'Vacant', role: 'Patrol Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '2S-08', rank: 'Sergeant', status: 'Vacant', role: 'Shift Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '2S-09', rank: 'Sergeant', status: 'Vacant', role: 'Shift Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-10', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-11', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-12', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-13', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-14', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '2V-15', rank: 'Corporal', status: 'Vacant', role: 'Road Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-10', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-11', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-12', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-13', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-14', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
+    { callSign: '2V-15', rank: 'Corporal', status: 'Vacant', role: 'Road Lead', startDate: '', fto: '', hours: '' },
     { callSign: '2U-16', rank: '1st Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '2U-17', rank: '1st Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '2U-18', rank: '1st Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
@@ -72,7 +72,7 @@ const staticBcsoRoster = [
     { callSign: '2U-27', rank: '2nd Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '2U-28', rank: '2nd Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '2U-29', rank: '2nd Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
-    { callSign: '2U-30', rank: '2nd Class Deputy', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
+    { callSign: '2U-30', rank: 'Probationary Deputy', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '2N-31', rank: 'Probationary Deputy', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '2N-32', rank: 'Probationary Deputy', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' }
 ];
@@ -80,8 +80,8 @@ const staticLspdRoster = [
     { callSign: '4C-01', rank: 'Chief', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
     { callSign: '4C-02', rank: 'Asst. Chief', status: 'Vacant', role: 'Department Head', startDate: '', fto: '', hours: '' },
     { callSign: '4C-03', rank: 'Captain', status: 'Vacant', role: 'Division Management', startDate: '', fto: '', hours: '' },
-    { callSign: '1L-04', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
-    { callSign: '4L-05', rank: 'Lieutenant', status: 'Vacant', role: 'LIA Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1C-04', rank: 'Lieutenant', status: 'Vacant', role: 'FTO Supervisor', startDate: '', fto: '', hours: '' },
+    { callSign: '1L-05', rank: 'Lieutenant', status: 'Vacant', role: 'LIA Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '1L-06', rank: 'Lieutenant', status: 'Vacant', role: 'TEU Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '4S-07', rank: 'Sergeant', status: 'Vacant', role: 'Patrol Supervisor', startDate: '', fto: '', hours: '' },
     { callSign: '4S-08', rank: 'Sergeant', status: 'Vacant', role: 'Shift Supervisor', startDate: '', fto: '', hours: '' },
@@ -106,7 +106,7 @@ const staticLspdRoster = [
     { callSign: '4U-27', rank: '2nd Class Officer', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '4U-28', rank: '2nd Class Officer', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
     { callSign: '4U-29', rank: '2nd Class Officer', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
-    { callSign: '4U-30', rank: '2nd Class Officer', status: 'Vacant', role: 'Patrol Trooper', startDate: '', fto: '', hours: '' },
+    { callSign: '4U-30', rank: 'Probationary Officer', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '4N-31', rank: 'Probationary Officer', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' },
     { callSign: '4N-32', rank: 'Probationary Officer', status: 'Vacant', role: 'Probationary', startDate: '', fto: '', hours: '' }
 ];
@@ -124,6 +124,8 @@ const fetchRosterData = async () => {
         ? {
             ...staticEntry,
             name: dynamicEntry.name,
+            rank: dynamicEntry.rank,
+            role: dynamicEntry.position,
             startDate: dynamicEntry.startDate,
             status: dynamicEntry.suspended === "false" ? "Active" : "Suspended",
             hours: dynamicEntry.hours
@@ -151,7 +153,7 @@ const renderTable = (roster) => (
         <th>Name</th>
         <th>Rank</th>
         <th>Status</th>
-        <th>Role</th>
+        <th>Role/Detail</th>
         <th>Start Date</th>
         <th>FTO</th>
         <th>Hours</th>
